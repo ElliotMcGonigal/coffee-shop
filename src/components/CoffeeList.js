26fbb95d6101b08f72908f373768b9838864beb2
Elliot McGonigal <elliot@elliotmcgonigal.com>
2022-10-30 04:56:15 -0700

    add Coffee List

--- src/components/CoffeeList.js
+++ src/components/CoffeeList.js
@@ -2,3 +2,29 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Coffee from './Coffee';
 
+function CoffeeList(props) {
+  return (
+    <React.Fragment>
+      <hr/>
+      {props.coffeeList.map((coffee) => 
+        <Coffee
+          whenCoffeeClicked={props.onCoffeeSelection}
+          name={coffee.name}
+          price={parseInt(coffee.price)}
+          origin={coffee.origin}
+          picture={coffee.picture}
+          roast={coffee.roast}
+          quantity={parseInt(coffee.quantity)}
+          id={coffee.id}
+          key={coffee.id}/>
+      )}
+    </React.Fragment>
+  );
+}
+
+CoffeeList.proptypes = {
+  onCoffeeSelection: PropTypes.func,
+  coffeeList: PropTypes.array
+};
+
+export default CoffeeList;
