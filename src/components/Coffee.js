c129ff8a800b90f5f471229a630eb69dc0e4b82d
Elliot McGonigal <elliot@elliotmcgonigal.com>
2022-10-29 22:31:01 -0700

    begin Coffee.js setup and add img folder with images

--- src/components/Coffee.js
+++ src/components/Coffee.js
@@ -1,1 +1,24 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
+
+function Coffee(props){
+  return (
+    <React.Fragment>
+      <div onClick = {() => props.whenCoffeeClicked(props.id)}>
+        <
+      </div>
+    </React.Fragment>
+  );
+}
+
+Coffee.propTypes = {
+  name: PropTypes.string,
+  origin: PropTypes.string,
+  price: PropTypes.number,
+  roast: PropTypes.string,
+  quantity: PropTypes.number,
+  id: PropTypes.string,
+  whenCoffeeClicked: PropTypes.func
+};
+
+export default Coffee;
